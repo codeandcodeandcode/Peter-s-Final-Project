b9a70ce5c979b19550cba1163a74a423254dd6c9
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-26 09:52:19 +0300

    Added a try and catch block to listen for any errors incase of any from the API

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ let pops =  document.querySelector(".popup");
 let usersCounter = 0;
 
 const allPosts = async () =>{
-
+    try{
+        const r = await fetch("https://dummyapi.io/data/v1/post?limit=9", {headers: headers});
+        const d = await r.json();
+        show(d);
+        lr.style.display = "none";
+    }
+    catch(err) {
+        console.log(err);
+        handleErrors();
+        lr.style.display = "none";
+    }
 
 
 }   
