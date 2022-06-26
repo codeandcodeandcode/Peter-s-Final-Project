7d10d4767a5ad83b0b8e272c2978ca8994be74c4
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-26 09:49:40 +0300

    Got all required containers and saved them in variables

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 window.addEventListener("load", ()=> {
+
+    const headers = {"app-id": "62a39b05e29594c9c6420196"};
     
-    const headers = {"app-id": "62a39b05e29594c9c6420196"};   
+let postContainer = document.querySelector("#a-b-c");
+let lr = document.getElementById("lr");
+let pops =  document.querySelector(".popup");
+let usersCounter = 0;
